8a1dbd94d3be7cc37b8c2786e14237ca84cf0873
angihe93 <angelhe903@gmail.com>
2025-06-09 16:50:51 -0400

    complete tutorial 3

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,69 +1,138 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText, type CoreMessage, appendResponseMessages, appendClientMessage, createIdGenerator, type Message } from 'ai';
 import { loadChat, saveChat } from '../../../tools/chat-store';
+import { z } from 'zod'
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
+const tools = {
+    // server-side tool with execute function:
+    getWeatherInformation: {
+        description: 'show the weather in a given city to the user',
+        parameters: z.object({ city: z.string() }),
+        execute: async ({ }: { city: string }) => {
+            const weatherOptions = ['sunny', 'cloudy', 'rainy', 'snowy', 'windy'];
+            return weatherOptions[
+                Math.floor(Math.random() * weatherOptions.length)
+            ];
+        },
+    },
+    // client-side tool that starts user interaction:
+    askForConfirmation: {
+        description: 'Ask the user for confirmation.',
+        parameters: z.object({
+            message: z.string().describe('The message to ask for confirmation.'),
+        }),
+    },
+    // client-side tool that is automatically executed on the client:
+    getLocation: {
+        description:
+            'Get the user location. Always ask for confirmation before using this tool.',
+        parameters: z.object({}),
+    },
+}
+
+export function errorHandler(error: unknown) {
+    if (error == null) {
+        return 'unknown error';
+    }
+
+    if (typeof error === 'string') {
+        return error;
+    }
+
+    if (error instanceof Error) {
+        return error.message;
+    }
+
+    return JSON.stringify(error);
+}
+
 export async function POST(req: Request) {
     // const { messages, id } = await req.json() //as { messages: CoreMessage[] }
     // get the last message from the client:
-    const { message, id } = await req.json() as { message: Message; id: string };
-    if (typeof id !== "string")
-        return new Response("Invalid id", { status: 400 });
-    if (typeof message !== "object")
-        return new Response("Invalid message", { status: 400 });
+    // const { message_, id_ } = await req.json();
+    // console.log(message_)
+    let body: any;
+    try { // works for tutorial 2 format
+        console.log("try")
+        body = await req.json();
+        const { message, id } = body as { message: Message; id: string };
+        // const { message, id } = await req.json() as { message: Message; id: string };
+        console.log(typeof message, message)
+        if (typeof id !== "string") {
+            throw new Error("Invalid id")
+            return new Response("Invalid id", { status: 400 });
+        }
 
-    // load the previous messages from the server:
-    const previousMessages = await loadChat(id);
+        if (typeof message !== "object") {
+            throw new Error("Invalid message")
+            return new Response("Invalid message", { status: 400 });
 
-    // append the new message to the previous messages:
-    const messages = appendClientMessage({
-        messages: previousMessages,
-        message,
-    });
+        }
 
-    const result = streamText({
-        // model: openai('gpt-4-turbo'),
-        model: openai('gpt-4o'),
-        system: 'You are a helpful assistant.',
-        messages: messages,
-        async onFinish({ response }) {
-            await saveChat({
-                id,
-                messages: appendResponseMessages({
-                    messages,
-                    responseMessages: response.messages,
-                }),
-            });
-        },
-        // id format for server-side messages:
-        experimental_generateMessageId: createIdGenerator({
-            prefix: 'msgs',
-            size: 16,
-        }),
-    });
+        // load the previous messages from the server:
+        const previousMessages = await loadChat(id);
 
-    // consume the stream to ensure it runs to completion & triggers onFinish
-    // even when the client response is aborted:
-    void result.consumeStream(); // no await
+        // append the new message to the previous messages:
+        const messages = appendClientMessage({
+            messages: previousMessages,
+            message,
+        });
 
-    return result.toDataStreamResponse({
-        sendReasoning: true, // Some models such as as DeepSeek deepseek-reasoner and Anthropic claude-3-7-sonnet-20250219 support reasoning tokens
-        getErrorMessage: error => {
-            if (error == null) {
-                return 'unknown error';
-            }
+        const result = streamText({
+            // model: openai('gpt-4-turbo'),
+            model: openai('gpt-4o'),
+            system: 'You are a helpful assistant.',
+            messages: messages,
+            async onFinish({ response }) {
+                await saveChat({
+                    id,
+                    messages: appendResponseMessages({
+                        messages,
+                        responseMessages: response.messages,
+                    }),
+                });
+            },
+            // id format for server-side messages:
+            experimental_generateMessageId: createIdGenerator({
+                prefix: 'msgs',
+                size: 16,
+            }),
+            tools: tools,
+            toolCallStreaming: true,
+            maxSteps: 5
+        });
 
-            if (typeof error === 'string') {
-                return error;
-            }
+        // consume the stream to ensure it runs to completion & triggers onFinish
+        // even when the client response is aborted:
+        void result.consumeStream(); // no await
 
-            if (error instanceof Error) {
-                return error.message;
-            }
+        return result.toDataStreamResponse({
+            sendReasoning: true, // Some models such as as DeepSeek deepseek-reasoner and Anthropic claude-3-7-sonnet-20250219 support reasoning tokens
+            getErrorMessage: errorHandler
+        })
 
-            return JSON.stringify(error);
-        },
-    })
+    } catch (error) { // works for tutorial 1 format
+        console.log("catch")
+        const messages = Array.isArray(body?.messages) ? body.messages : [];
+        // const { messages } = await req.json() as { messages: CoreMessage[] }
+        console.log(typeof messages, messages)
+        const result = streamText({
+            // model: openai('gpt-4-turbo'),
+            model: openai('gpt-4o'),
+            system: 'You are a helpful assistant.',
+            messages: messages,
+            tools: tools,
+            toolCallStreaming: true,
+            maxSteps: 5
+        });
+
+        return result.toDataStreamResponse({
+            sendReasoning: true, // Some models such as as DeepSeek deepseek-reasoner and Anthropic claude-3-7-sonnet-20250219 support reasoning tokens
+            getErrorMessage: errorHandler
+        });
+
+    }
 }
--- src/components/ui/chat.tsx
+++ src/components/ui/chat.tsx
@@ -7,7 +7,7 @@ export default function Chat({
     id,
     initialMessages,
 }: { id?: string | undefined; initialMessages?: Message[] } = {}) {
-    const { input, handleInputChange, handleSubmit, messages } = useChat({
+    const { input, handleInputChange, handleSubmit, messages, addToolResult } = useChat({
         id, // use the provided chat ID
         initialMessages, // initial messages if provided
         sendExtraMessageFields: true, // send id and createdAt for each message
@@ -20,25 +20,170 @@ export default function Chat({
         experimental_prepareRequestBody({ messages, id }) {
             return { message: messages[messages.length - 1], id };
         },
+        maxSteps: 5,
+        async onToolCall({ toolCall }) {
+            if (toolCall.toolName === 'getLocation') {
+                const cities = [
+                    'New York',
+                    'Los Angeles',
+                    'Chicago',
+                    'San Francisco',
+                ];
+                return cities[Math.floor(Math.random() * cities.length)];
+            }
+        },
     });
 
     // simplified rendering code, extend as needed:
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center ">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-                messages
-                {messages.map(m => (
-                    <div key={m.id}>
-                        {m.role === 'user' ? 'User: ' : 'AI: '}
-                        {m.content}
-                    </div>
-                ))}
+    // return (
+    //     <main className="flex min-h-screen flex-col items-center justify-center ">
+    //         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+    //             messages
+    //             {messages.map(m => (
+    //                 <div key={m.id}>
+    //                     <strong>{m.role === 'user' ? 'User: ' : 'AI: '}</strong>
+    //                     {m.content}
+    //                 </div>
+    //             ))}
 
-                <form onSubmit={handleSubmit}>
-                    {/* press enter to submit */}
-                    <input value={input} onChange={handleInputChange} className='border' />
-                </form>
-            </div>
-        </main>
+    //             <form onSubmit={handleSubmit}>
+    //                 {/* press enter to submit */}
+    //                 <input value={input} onChange={handleInputChange} className='border' />
+    //             </form>
+    //         </div>
+    //     </main>
+    // );
+
+    // tutorial 3
+    return (
+        <>
+            <main className="flex min-h-screen flex-col items-center justify-center ">
+                <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+                    messages
+                    {messages?.map(message => (
+                        <div key={message.id}>
+                            <strong>{`${message.role}: `}</strong>
+                            {message.parts.map((part, index) => {
+                                switch (part.type) {
+                                    case 'step-start':
+                                        // show step boundaries as horizontal lines:
+                                        return index > 0 ? (
+                                            <div key={index} className="text-gray-500">
+                                                <hr className="my-2 border-gray-300" />
+                                            </div>
+                                        ) : null;
+
+                                    // render text parts as simple text:
+                                    case 'text':
+                                        return part.text;
+
+                                    // for tool invocations, distinguish between the tools and the state:
+                                    case 'tool-invocation': {
+                                        const callId = part.toolInvocation.toolCallId;
+
+                                        switch (part.toolInvocation.toolName) {
+                                            case 'askForConfirmation': {
+                                                switch (part.toolInvocation.state) {
+                                                    case 'call':
+                                                        return (
+                                                            <div key={callId}>
+                                                                {part.toolInvocation.args.message}
+                                                                <div>
+                                                                    <button
+                                                                        onClick={() =>
+                                                                            addToolResult({
+                                                                                toolCallId: callId,
+                                                                                result: 'Yes, confirmed.',
+                                                                            })
+                                                                        }
+                                                                    >
+                                                                        Yes
+                                                                    </button>
+                                                                    <button
+                                                                        onClick={() =>
+                                                                            addToolResult({
+                                                                                toolCallId: callId,
+                                                                                result: 'No, denied',
+                                                                            })
+                                                                        }
+                                                                    >
+                                                                        No
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                        );
+                                                    case 'result':
+                                                        return (
+                                                            <div key={callId}>
+                                                                Location access allowed:{' '}
+                                                                {part.toolInvocation.result}
+                                                            </div>
+                                                        );
+                                                }
+                                                break;
+                                            }
+
+                                            case 'getLocation': {
+                                                switch (part.toolInvocation.state) {
+                                                    case 'call':
+                                                        return <div key={callId}>Getting location...</div>;
+                                                    case 'result':
+                                                        return (
+                                                            <div key={callId}>
+                                                                Location: {part.toolInvocation.result}
+                                                            </div>
+                                                        );
+                                                }
+                                                break;
+                                            }
+
+                                            case 'getWeatherInformation': {
+                                                switch (part.toolInvocation.state) {
+                                                    // example of pre-rendering streaming tool calls:
+                                                    case 'partial-call':
+                                                        return (
+                                                            <pre key={callId}>
+                                                                {JSON.stringify(part.toolInvocation, null, 2)}
+                                                            </pre>
+                                                        );
+                                                    case 'call':
+                                                        return (
+                                                            <div key={callId}>
+                                                                Getting weather information for{' '}
+                                                                {part.toolInvocation.args.city}...
+                                                            </div>
+                                                        );
+                                                    case 'result':
+                                                        return (
+                                                            <div key={callId}>
+                                                                Weather in {part.toolInvocation.args.city}:{' '}
+                                                                {part.toolInvocation.result}
+                                                            </div>
+                                                        );
+                                                }
+                                                break;
+                                            }
+                                        }
+                                        switch (part.toolInvocation.state) {
+                                            case 'partial-call':
+                                                return <>render partial tool call</>;
+                                            case 'call':
+                                                return <>render full tool call</>;
+                                            case 'result':
+                                                return <>render tool result</>;
+                                        }
+                                    }
+                                }
+                            })}
+                            <br />
+                        </div>
+                    ))}
+
+                    <form onSubmit={handleSubmit}>
+                        <input value={input} onChange={handleInputChange} className='border' />
+                    </form>
+                </div>
+            </main>
+        </>
     );
 }
