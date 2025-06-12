6069706bcdba765aa0c326d5f68b4368d94ff7ae
angihe93 <angelhe903@gmail.com>
2025-06-12 12:24:30 -0400

    login loading spinner

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -28,6 +28,8 @@ import {
 // import { Input } from "@/components/ui/input"
 import { toast } from "sonner"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -39,6 +41,8 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+
+  const [isLoading, setIsLoading] = useState(false)
 
   const router = useRouter()
   // https://ui.shadcn.com/docs/components/form
@@ -52,6 +56,7 @@ export function LoginForm({
   })
   // Define submit handler
   async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true)
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
@@ -62,6 +67,7 @@ export function LoginForm({
     } else {
       toast.error(message as string)
     }
+    setIsLoading(false)
   }
 
   return (
@@ -70,7 +76,7 @@ export function LoginForm({
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
           <CardDescription>
-            Login with your Apple or Google account
+            Login with your Google account
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -79,15 +85,6 @@ export function LoginForm({
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
               <div className="grid gap-6">
                 <div className="flex flex-col gap-4">
-                  <Button variant="outline" className="w-full">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                      <path
-                        d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
-                        fill="currentColor"
-                      />
-                    </svg>
-                    Login with Apple
-                  </Button>
                   <Button variant="outline" className="w-full">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                       <path
@@ -106,12 +103,6 @@ export function LoginForm({
                 <div className="grid gap-6">
                   <div className="grid gap-3">
                     <Label htmlFor="email">Email</Label>
-                    {/* <Input
-                    id="email"
-                    type="email"
-                    placeholder="m@example.com"
-                    required
-                  /> */}
                     <FormField
                       control={form.control}
                       name="email"
@@ -151,11 +142,12 @@ export function LoginForm({
                       )}
                     />
                   </div>
-                  <Button type="submit" className="w-full">
-                    {/* test: */}
-                    {/* <Button type="button" className="w-full" onClick={signUp}> */}
-                    {/* <Button type="button" className="w-full" onClick={()=>{onSubmit()}}> */}
-                    Login
+                  <Button type="submit" className="w-full" disabled={isLoading}>
+                    {isLoading ? (
+                      <Loader2 className="size-4 animate-spin" />
+                    ) : (
+                      "Login"
+                    )}
                   </Button>
                 </div>
                 <div className="text-center text-sm">
