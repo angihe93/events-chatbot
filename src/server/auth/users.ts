09f166607d2252132bf06204def3dc80d74307a8
angihe93 <angelhe903@gmail.com>
2025-06-12 12:18:12 -0400

    sign in working

--- src/server/auth/users.ts
+++ src/server/auth/users.ts
@@ -1,13 +1,28 @@
 "use server";
 import { auth } from "~/lib/auth"
 
-export const signIn = async () => {
-    await auth.api.signInEmail({
-        body: {
-            email: "user@email.com",
-            password: "password",
+export const signIn = async (email: string, password: string) => {
+    try {
+        await auth.api.signInEmail({
+            body: {
+                email,
+                password
+            }
+        })
+        console.log("signed in")
+        return {
+            success: true,
+            message: "Signed in successfully"
         }
-    })
+    }
+    catch (error) {
+        const e = error as Error
+        console.log("Error signing in", error)
+        return {
+            success: false,
+            message: e.message || "an unknown error occurred"
+        }
+    }
 }
 
 export const signUp = async () => {
