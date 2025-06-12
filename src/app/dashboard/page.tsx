3e633c9fc3a61c9b1025e621505ea5bb0cc19613
angihe93 <angelhe903@gmail.com>
2025-06-12 14:12:15 -0400

    logout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import { Logout } from "~/components/logout"
+
 export default function Dashboard() {
-    return <h1>Dashboard</h1>
+    return (<>
+        <h1>Dashboard</h1>
+        <Logout />
+    </>)
+
 }
